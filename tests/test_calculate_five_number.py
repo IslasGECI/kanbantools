d425e35645c9bf07fa79f1ddc47e8a54d29502b1
Maritza <maritza.bello@islas.org.mx>
2020-06-19 14:23:27 -0700

    Agrega contenido de pruebas

--- tests/test_calculate_five_number.py
+++ tests/test_calculate_five_number.py
@@ -8,11 +8,14 @@ class TestFiveNumbers(unittest.TestCase):
         Crea variables que se usarán en las pruebas
         """
         seed(1)
+        self.data=[1,2,3,4,5]
+        self_expected_output=[2,3,4]
 
-    def test_quantiles_25_50_76(self):
+    def test_quantiles_25_50_75(self):
         """
         Sacar los cuantiles 25, 50 y 75% del numero de tareas de clase 3
         """
+        self.assertEqual(quantiles_25_50_75(self.data), self.expected_output)
 
 if __name__ == '__main__':
     unittest.main()
