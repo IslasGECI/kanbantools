fbb868bdb42476a4141871ba3b8a4f89dc5463a1
Maritza <maritza.bello@islas.org.mx>
2020-06-19 14:31:05 -0700

    Prueba intervalo de credibilidad

--- tests/test_calculate_five_number.py
+++ tests/test_calculate_five_number.py
@@ -10,12 +10,20 @@ class TestFiveNumbers(unittest.TestCase):
         seed(1)
         self.data=[1,2,3,4,5]
         self_expected_output=[2,3,4]
+        self.data_101=[1:101]
+        self.expected_output_101=[96]
 
     def test_quantiles_25_50_75(self):
         """
         Sacar los cuantiles 25, 50 y 75% del numero de tareas de clase 3
         """
         self.assertEqual(quantiles_25_50_75(self.data), self.expected_output)
+
+    def test_ic_95(self):
+        """
+        Sacar los cuantiles 25, 50 y 75% del numero de tareas de clase 3
+        """
+        self.assertEqual(ic_95(self.data_101), self.expected_output_101)
 
 if __name__ == '__main__':
     unittest.main()
