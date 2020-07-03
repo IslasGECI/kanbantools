3d43311a914692a424a6d71548ded6801ea19a9e
Fernando <fernando.alvarez@islas.org.mx>
2020-07-03 15:04:03 -0700

    agrega funciones para pasar las pruebas

--- tests/test_calculate_five_number.py
+++ tests/test_calculate_five_number.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from kanban_tools import *
+from kanban_tools import calculate_five_number
 from random import seed
 
 
@@ -11,22 +11,22 @@ class TestFiveNumbers(unittest.TestCase):
         """
         seed(1)
         self.data = [1, 2, 3, 4, 5]
-        self_expected_output = [2, 3, 4]
-        self.data_101: np.array = np.arrange(101)
-        self.expected_output_101: np.array = np.array([3.5, 98.5])
+        self.expected_output = [2, 3, 4]
+        self.data_101: np.array = np.arange(101)
+        self.expected_output_101: np.array = np.array([2.5, 97.5])
 
     def test_quantiles_25_50_75(self):
         """
         Sacar los cuantiles 25, 50 y 75% del numero de tareas de clase 3
         """
-        self.assertEqual(quantiles_25_50_75(self.data), self.expected_output)
+        np.testing.assert_array_equal(calculate_five_number.quantiles_25_50_75(self.data), self.expected_output)
 
     def test_ic_95(self):
         """
         Sacar los cuantiles 25, 50 y 75% del numero de tareas de clase 3
         """
-        self.assertEqual(ic_95(self.data_101), self.expected_output_101)
+        np.testing.assert_array_equal(calculate_five_number.ic_95(self.data_101), self.expected_output_101)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.main()
