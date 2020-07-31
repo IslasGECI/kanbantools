b9aea1f3f0c30b1418eaac19a537b853e97c8811
Nepo <braulio.rojas@islas.org.mx>
2020-07-31 14:32:15 -0700

    Da formato con black

--- tests/test_calculate_five_number.py
+++ tests/test_calculate_five_number.py
@@ -10,8 +10,9 @@ expected_output = [2, 3, 4]
 data_101: np.array = np.arange(101)
 expected_output_101: np.array = np.array([2.5, 97.5])
 expected_five_number_101: np.array([0, 25, 50, 75, 100])
-d: dict = {'col1': [1, 2], 'col2': [3, 4]}
+d: dict = {"col1": [1, 2], "col2": [3, 4]}
 df: pd.DataFrame = pd.DataFrame(data=d)
+
 
 def test_quantiles_25_50_75():
     """
@@ -26,16 +27,19 @@ def test_ic_95():
     """
     np.testing.assert_array_equal(calculate_five_number.ic_95(data_101), expected_output_101)
 
+
 def test_five_number():
-    np.testing.assert_array_equal(calculate_five_number.five_number(data),data)
-    np.testing.assert_array_equal(calculate_five_number.five_number(data_101),expected_five_number_101)
+    np.testing.assert_array_equal(calculate_five_number.five_number(data), data)
+    np.testing.assert_array_equal(
+        calculate_five_number.five_number(data_101), expected_five_number_101
+    )
 
 
 def test_table_AED():
     tabla_aed = calculate_five_number.table_AED(df)
     index = tabla_aed.index
-    number_of_rows = len(index) 
+    number_of_rows = len(index)
     assert number_of_rows == 2
     index = tabla_aed.columns
-    number_of_columns = len(index) 
+    number_of_columns = len(index)
     assert number_of_columns == 9
