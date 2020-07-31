8776596e4a5794fe8ce7975ccb3e840152fe3bca
Nepo <braulio.rojas@islas.org.mx>
2020-07-31 14:21:27 -0700

    Agrega prueba para los cinco números

--- tests/test_calculate_five_number.py
+++ tests/test_calculate_five_number.py
@@ -8,6 +8,7 @@ data = [1, 2, 3, 4, 5]
 expected_output = [2, 3, 4]
 data_101: np.array = np.arange(101)
 expected_output_101: np.array = np.array([2.5, 97.5])
+expected_five_number_101: np.array([0, 25, 50, 75, 100])
 
 
 def test_quantiles_25_50_75():
@@ -19,6 +20,12 @@ def test_quantiles_25_50_75():
 
 def test_ic_95():
     """
-    Sacar los cuantiles 25, 50 y 75% del numero de tareas de clase 3
+    Sacar los cuantiles intervalo de credibilidad del 95 porciento
     """
     np.testing.assert_array_equal(calculate_five_number.ic_95(data_101), expected_output_101)
+
+def test_five_number():
+    np.testing.assert_array_equal(calculate_five_number.five_number(data),data)
+    np.testing.assert_array_equal(calculate_five_number.five_number(data_101),expected_five_number_101)
+
+
