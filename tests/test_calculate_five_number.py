3cfc5cddb9b4a7be47dabd82a0d1983851b7cbc8
Maritza <maritza.bello@islas.org.mx>
2020-09-11 14:35:47 -0700

    Agrega prueba para función que genera la tabla de AED

--- tests/test_calculate_five_number.py
+++ tests/test_calculate_five_number.py
@@ -12,6 +12,7 @@ expected_output_101: np.array = np.array([2.5, 97.5])
 expected_five_number_101: np.array = np.array([0, 25, 50, 75, 100])
 d: dict = {"col1": [1, 2, 5, 8, 7]}
 df: pd.DataFrame = pd.DataFrame(data=d)
+column_name_expected = ["Nombre_de_la_categoria", "Cantidad_de_datos", "Promedio", "Desviacion_estandar", "Minimo_de_la_muestra", "Cuartil_inferior", "Valor_medio", "Cuartil_superior","Maximo_de_la_muestra"]
 
 
 def test_quantiles_25_50_75():
@@ -36,6 +37,9 @@ def test_five_number():
 
 
 def test_table_AED():
+    """
+    Revisa la forma y nombre de las columnas del data frame del AED
+    """
     tabla_aed = calculate_five_number.table_AED(df)
     index = tabla_aed.index
     number_of_rows = len(index)
@@ -43,3 +47,6 @@ def test_table_AED():
     index = tabla_aed.columns
     number_of_columns = len(index)
     assert number_of_columns == 9
+    assert tabla_aed.ilog[0,0] == "col1"
+    column_name_obtained = tabla_aed.columns.values.tolist()
+    assert column_name_expected == column_name_obtained
